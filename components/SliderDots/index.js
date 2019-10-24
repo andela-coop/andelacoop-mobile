9b0d87ab7b1803f20b0384568efdaf54ca1b061b
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2019-10-24 13:00:29 +0100

    abstract slider dots style

--- components/SliderDots/index.js
+++ components/SliderDots/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { View } from "react-native"
+import styles from './styles'
 
 const andelaBlue = '#3359DB';
 const inactiveGrey = '#DAE1E7';
@@ -7,28 +8,17 @@ const inactiveGrey = '#DAE1E7';
  * Renders slider dots
  *
  * @param length The number of dots to render
- * @param activeDot The active slide
+ * @param activeDot The active dot
  * @param continuous This decides if only the active dot is displayed as active or
  * if all dots before and incluing the active dot is displayed as active
  */
 export default ({ length, activeDot, continuous }) => {
   const fauxArray = new Array(length).fill('');
-  const containerStyle = {
-    flexDirection: 'row',
-    width: 70,
-    justifyContent: 'space-between'
-  };
-  const dotStyle = {
-    height: 10,
-    width: 10,
-    borderRadius: 50,
-    marginBottom: 50
-  };
   
-  return <View style={containerStyle}>
+  return <View style={styles.containerStyle}>
     {fauxArray.map((item, index) => <View
       style={{
-        ...dotStyle,
+        ...styles.dotStyle,
         backgroundColor:
           continuous
             ? activeDot >= index
---
+++ components/SliderDots/styles.js
@@ -0,0 +1,15 @@
+import { Dimensions, StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  containerStyle: {
+    flexDirection: 'row',
+    width: 70,
+    justifyContent: 'space-between'
+  }, 
+  dotStyle: {
+    height: 10,
+    width: 10,
+    borderRadius: 50,
+    marginBottom: 50
+  }
+});
